7c5a083d3153afe8d47f4e695ab2e24fae46b9d5
Joana Figueira <joana.figueira@somenergia.coop>
2019-04-03 15:36:32 +0200

    projectcontroller.test_getattr

--- solarprosumers/projectcontroller.py
+++ solarprosumers/projectcontroller.py
@@ -1,3 +1,6 @@
+class ProjectController:
+	def __init__(self, is_paid):
+		self.is_paid = True
 
 
 # vim: noet ts=4 sw=4
--- solarprosumers/projectcontroller_test.py
+++ solarprosumers/projectcontroller_test.py
@@ -4,8 +4,18 @@ import unittest
 
 class ProjectController_Test(unittest.TestCase):
 
-	def test(self):
-		self.fail("it works")
+	def setUp(self):
+		self.maxDiff = None
+
+	def setupProject(self, **kwds):
+		project = projectcontroller.ProjectController(**kwds)
+		return project
+
+	def test_getattr(self):
+		proj = self.setupProject(
+			is_paid = True,
+			)
+		self.assertEqual(proj.is_paid, True)
 
 
 # vim: noet ts=4 sw=4
