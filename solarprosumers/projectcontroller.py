2e54921eec58fa2ba1f311694a0bb74c11657293
Joana Figueira <joana.figueira@somenergia.coop>
2019-04-03 17:19:51 +0200

    indentation with spaces

--- solarprosumers/projectcontroller.py
+++ solarprosumers/projectcontroller.py
@@ -3,48 +3,48 @@ from yamlns import namespace as ns
 
 class ProjectController:
 
-	_allowed = [
-		'is_paid',
-		'registration_date',
-	]
+    _allowed = [
+        'is_paid',
+        'registration_date',
+    ]
 
-	def _checkAttribute(self, name):
-		if name not in self._allowed:
-			raise AttributeError(name)
+    def _checkAttribute(self, name):
+        if name not in self._allowed:
+            raise AttributeError(name)
 
-	def __init__(self, **kwds):
-		self._changes = ns()
-		self._values = ns(kwds)
-		for name in kwds:
-			self._checkAttribute(name)
+    def __init__(self, **kwds):
+        self._changes = ns()
+        self._values = ns(kwds)
+        for name in kwds:
+            self._checkAttribute(name)
 
-	def __setattr__(self, name, value):
-		if not name.startswith('_'):
-			self._checkAttribute(name)
-			self._changes[name]=value
-			self._values[name]=value
-		return super(
-			ProjectController, self).__setattr__(name, value)
+    def __setattr__(self, name, value):
+        if not name.startswith('_'):
+            self._checkAttribute(name)
+            self._changes[name]=value
+            self._values[name]=value
+        return super(
+            ProjectController, self).__setattr__(name, value)
 
-	def __getattr__(self, name):
-		self._checkAttribute(name)
-		return self._values[name]
+    def __getattr__(self, name):
+        self._checkAttribute(name)
+        return self._values[name]
 
-	def changes(self):
-		return self._changes
-
-
-	def preregister(self,
-			current_date,
-			member_id,
-			contract_id,
-			campaign_id,
-			):
-		self.is_paid = False
-		self.registration_date = current_date
+    def changes(self):
+        return self._changes
 
 
+    def preregister(self,
+            current_date,
+            member_id,
+            contract_id,
+            campaign_id,
+            ):
+        self.is_paid = False
+        self.registration_date = current_date
 
 
 
-# vim: noet ts=4 sw=4
+
+
+# vim: et ts=4 sw=4
--- solarprosumers/projectcontroller_test.py
+++ solarprosumers/projectcontroller_test.py
@@ -10,68 +10,68 @@ def isodate(isodatestring):
 
 class StateController_Test(unittest.TestCase):
 
-	def assertExceptionMessage(self, e, text):
-		self.assertEqual(forceUnicode(e.args[0]), text)
+    def assertExceptionMessage(self, e, text):
+        self.assertEqual(forceUnicode(e.args[0]), text)
 
-	def setUp(self):
-		self.maxDiff = None
+    def setUp(self):
+        self.maxDiff = None
 
-	def test_init_withNiceAttributes(self):
-		proj = ProjectController(
-			is_paid = True,
-			)
-		self.assertEqual(proj.is_paid, True)
-		self.assertEqual(proj.changes(), ns())
+    def test_init_withNiceAttributes(self):
+        proj = ProjectController(
+            is_paid = True,
+            )
+        self.assertEqual(proj.is_paid, True)
+        self.assertEqual(proj.changes(), ns())
 
-	def test_init_withBadAttribute(self):
-		with self.assertRaises(AttributeError) as ctx:
-			proj = ProjectController(
-				im_bad = True,
-				)
-		self.assertEqual(format(ctx.exception), "im_bad")
+    def test_init_withBadAttribute(self):
+        with self.assertRaises(AttributeError) as ctx:
+            proj = ProjectController(
+                im_bad = True,
+                )
+        self.assertEqual(format(ctx.exception), "im_bad")
 
 
-	def test_getattr_badAttribute(self):
-		proj = ProjectController()
-		with self.assertRaises(AttributeError) as ctx:
-			value = proj.im_bad
-		self.assertEqual(format(ctx.exception), "im_bad")
+    def test_getattr_badAttribute(self):
+        proj = ProjectController()
+        with self.assertRaises(AttributeError) as ctx:
+            value = proj.im_bad
+        self.assertEqual(format(ctx.exception), "im_bad")
 
-	def test_setattr_badAttribute(self):
-		proj = ProjectController()
-		with self.assertRaises(AttributeError) as ctx:
-			proj.im_bad = True
-		self.assertEqual(format(ctx.exception), "im_bad")
+    def test_setattr_badAttribute(self):
+        proj = ProjectController()
+        with self.assertRaises(AttributeError) as ctx:
+            proj.im_bad = True
+        self.assertEqual(format(ctx.exception), "im_bad")
 
-	def test_setattr_properAttribute(self):
-		proj = ProjectController(is_paid=False)
-		proj.is_paid = True
-		self.assertEqual(proj.is_paid, True)
-		self.assertEqual(proj.changes(), ns(is_paid=True))
+    def test_setattr_properAttribute(self):
+        proj = ProjectController(is_paid=False)
+        proj.is_paid = True
+        self.assertEqual(proj.is_paid, True)
+        self.assertEqual(proj.changes(), ns(is_paid=True))
 
 
 class ProjectController_Test(unittest.TestCase):
 
-	from .testutils import assertNsEqual
+    from .testutils import assertNsEqual
 
-	def test_preregister(self):
-		proj = ProjectController()
-		proj.preregister(
-			current_date = isodate('2019-02-01'),
-			member_id = 200,
-			contract_id = None, # 400
-			campaign_id = 2,
-		)
-		self.assertNsEqual(proj.changes(), """
-is_paid: false
-registration_date: 2019-02-01
-""")
-
-
+    def test_preregister(self):
+        proj = ProjectController()
+        proj.preregister(
+            current_date = isodate('2019-02-01'),
+            member_id = 200,
+            contract_id = None, # 400
+            campaign_id = 2,
+        )
+        self.assertNsEqual(proj.changes(), """
+            is_paid: false
+            registration_date: 2019-02-01
+        """)
 
 
 
 
 
 
-# vim: noet ts=4 sw=4
+
+
+# vim: et ts=4 sw=4
