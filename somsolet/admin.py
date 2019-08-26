f5183d1b8c5e6823f298a8719aad99fd9b1595c0
Joana Figueira <joana.figueira@somenergia.coop>
2019-08-26 17:27:18 +0200

    Update columns to be imported with admin import-export

--- somsolet/admin.py
+++ somsolet/admin.py
@@ -1,33 +1,55 @@
-from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
-from .models import Project, Campaign, Client, Technical_details, Engineering
 
-#admin.site.register(Project)
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
+from import_export.widgets import ForeignKeyWidget
+
+from .models import Project, Campaign, Client, Technical_details, Engineering, Technical_campaign
+
+
+class ProjectResource(resources.ModelResource):
+    name = fields.Field(
+        attribute='name',
+        column_name='instalació')
+    campaign = fields.Field(
+        attribute='campaign',
+        column_name="campanya",
+        widget=ForeignKeyWidget(Campaign, 'name'))
+    client = fields.Field(
+        attribute='client',
+        column_name="Número de soci/a de Som Energia",
+        widget=ForeignKeyWidget(Client, 'membership_number'))
+
+    class Meta:
+        model = Project
+        import_id_fields = ('name', 'campaign', 'client')
+        exclude = ('id', )
+
 
 @admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
+class ProjectAdmin(ImportExportModelAdmin):
     list_display = ('campaign', 'client', 'status', 'warning')
-    list_filter = ('campaign','status', 'discarded_type')
+    list_filter = ('campaign', 'status', 'discarded_type')
+    resource_class = ProjectResource
 
 
-    fieldsets = (
-        (None, {
-            'fields': ('campaign', 'status')
-        }),
-        ('Execution details', {
-            'fields': ('is_invalid_report', 'is_signed') #just for testing... add relevant fields
-            #'fields': ('date_start_installation', 'engineering')
-        }),
-    )
-
-class CampaignInline(admin.TabularInline):
+class InstallationsInline(admin.TabularInline):
     model = Project
+
 
 @admin.register(Campaign)
 class CampaignAdmin(ImportExportModelAdmin):
-    list_display = ('name', 'engineering', 'autonomous_community', 'date_warranty_payment')
-    list_filter = ('engineering', 'autonomous_community')
-    inlines = [CampaignInline]
+    list_display = (
+        'name',
+        'engineering',
+        'autonomous_community',
+        'date_warranty_payment'
+    )
+    list_filter = (
+        'engineering',
+        'autonomous_community'
+    )
+    inlines = [InstallationsInline]
 
 
 @admin.register(Technical_campaign)
@@ -37,42 +59,96 @@ class Technical_CampaignAdmin(ImportExportModelAdmin):
 
 @admin.register(Engineering)
 class EngineeringAdmin(ImportExportModelAdmin):
-    pass
+    list_display = ('name', 'phone_number', 'email')
 
 
 class Technical_detailsResource(resources.ModelResource):
-    comments = fields.Field(attribute='comments', column_name='COMENTARIOS')
+    project = fields.Field(
+        attribute='project',
+        column_name="instalació",
+        widget=ForeignKeyWidget(Project, 'name'))
+    campaign = fields.Field(
+        attribute='campaign',
+        column_name="campanya",
+        widget=ForeignKeyWidget(Campaign, 'name'))
+    client = fields.Field(
+        attribute='client',
+        column_name="Número de DNI",
+        widget=ForeignKeyWidget(Client, 'dni'))
+    administrative_division = fields.Field(
+        attribute='administrative_division',
+        column_name="Comarca")
+    municipality = fields.Field(
+        attribute='municipality',
+        column_name="Municipi")
+    street = fields.Field(
+        attribute='street',
+        column_name="Adreça")
+    contract_number = fields.Field(
+        attribute='contract_number',
+        column_name="Número de contracte amb Som Energia")
+    cups = fields.Field(
+        attribute='cups',
+        column_name="CUPS - Codi Unificat del Punt de Subministrament")
+    voltage = fields.Field(
+        attribute='voltage',
+        column_name="Tipus d'instal·lació")
+    tariff = fields.Field(
+        attribute='tariff',
+        column_name="Tarifa d'accès")
+    anual_consumption = fields.Field(
+        attribute='anual_consumption',
+        column_name="Selecciona l'ús anual d'electricitat d'aquest habitatge o local")
+    installation_singlephase_model = fields.Field(
+        attribute='installation_singlephase_model',
+        column_name="Model monofàsic triat")
+    installation_threephase_model = fields.Field(
+        attribute='installation_threephase_model',
+        column_name="Model trifàsic triat")
+    acquire_interest = fields.Field(
+        attribute='acquire_interest',
+        column_name="Estic interessat en adquirir")
+    comments = fields.Field(
+        attribute='comments',
+        column_name="COMENTARIOS")
 
     class Meta:
         model = Technical_details
+        import_id_fields = ('project', 'campaign', 'client')
+        exclude = ('id', )
 
 
 @admin.register(Technical_details)
 class Technical_detailsAdmin(ImportExportModelAdmin):
+    list_display = ('campaign', 'project', 'client')
+    list_filter = ('campaign', 'client')
     resource_class = Technical_detailsResource
 
 
 class ClientResource(resources.ModelResource):
     name = fields.Field(
         attribute='name',
-        column_name='Nombre y apellidos')
+        column_name='Nom i cognoms')
     membership_number = fields.Field(
         attribute='membership_number',
-        column_name='Número de socio/a de Som Energia')
+        column_name='Número de soci/a de Som Energia')
     dni = fields.Field(
         attribute='dni',
         column_name='Número de DNI')
     phone_number = fields.Field(
         attribute='phone_number',
-        column_name='Teléfono de contacto')
+        column_name='Telèfon de contacte')
     email = fields.Field(
         attribute='email',
-        column_name='Correo electrónico')
+        column_name='Correu electrònic')
 
     class Meta:
         model = Client
+        import_id_fields = ('name', 'membership_number', 'dni')
+        exclude = ('id', )
 
 
 @admin.register(Client)
 class ClientAdmin(ImportExportModelAdmin):
+    list_display = ('name', 'membership_number', 'email')
     resource_class = ClientResource
