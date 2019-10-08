e96ef179a6b34f643ba42868a7bd617636c36bd1
Joana Figueira <joana.figueira@somenergia.coop>
2019-10-08 17:06:18 +0200

    Add: prettify login, logout, password reset forms

--- config/settings/base.py
+++ config/settings/base.py
@@ -90,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGIN_REDIRECT_URL = '/somsolet/profile_engineering/'
-
+LOGOUT_REDIRECT_URL = 'login'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
