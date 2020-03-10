9e3df77a96a3d5a074f46a4871a0f6693626b980
Marta <marta.fernandez@somenergia.coop>
2020-03-10 15:16:28 +0100

    test_signature_auth_invalid_status_condition

--- somsolet/tests/test_views.py
+++ somsolet/tests/test_views.py
@@ -7,7 +7,7 @@ from factories import ProjectFactory
 from mixer.backend.django import mixer
 from mock import patch
 from somsolet.views import (PrereportView, ProjectView, TechnicalVisitView,
-                            ReportView, OfferView)
+                            ReportView, OfferView, SignatureView)
 
 
 @pytest.mark.django_db
@@ -306,3 +306,25 @@ class TestViews:
 
             response = SignatureView.as_view()(request, pk=1)
             assert response.status_code == 200
+
+    def test_signature_auth_invalid_status_condition(self):
+        project = ProjectFactory.build()
+
+        get_initial_mock = {
+            'campaign': project.campaign,
+            'project': project.id,
+            'client': project.client,
+            'status': 'random',
+            'campaign_pk': 2
+        }
+        with patch.object(
+            SignatureView,
+            'get_initial',
+            return_value=get_initial_mock
+        ):
+            path = reverse('technical_visit', kwargs={'pk': 1})
+            request = RequestFactory().get(path)
+            request.user = mixer.blend(User)
+
+            response = SignatureView.as_view()(request, pk=1)
+            assert 'project' in response.url
