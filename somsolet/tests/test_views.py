80a5a9321a2305d7f9c571ed6306e2f24b5686f3
Marta <marta.fernandez@somenergia.coop>
2020-03-10 11:27:45 +0100

    test_report_view_auth_invalid_status_condition

--- somsolet/tests/test_views.py
+++ somsolet/tests/test_views.py
@@ -178,3 +178,25 @@ class TestViews:
 
             response = ReportView.as_view()(request, pk=1)
             assert response.status_code == 200
+
+    def test_report_view_auth_invalid_status_condition(self):
+        project = ProjectFactory.build()
+
+        get_initial_mock = {
+            'campaign': project.campaign,
+            'project': project.id,
+            'client': project.client,
+            'status': 'random',
+            'campaign_pk': 2
+        }
+        with patch.object(
+            ReportView,
+            'get_initial',
+            return_value=get_initial_mock
+        ):
+            path = reverse('technical_visit', kwargs={'pk': 1})
+            request = RequestFactory().get(path)
+            request.user = mixer.blend(User)
+
+            response = ReportView.as_view()(request, pk=1)
+            assert 'project' in response.url
