71a077fcc45c701f59d46675b41ba45cf19274c1
Marta <marta.fernandez@somenergia.coop>
2020-03-10 11:20:24 +0100

    test_technical_visit_unauthenticated

--- somsolet/tests/test_views.py
+++ somsolet/tests/test_views.py
@@ -135,3 +135,24 @@ class TestViews:
 
             response = TechnicalVisitView.as_view()(request, pk=1)
             assert 'project' in response.url
+
+    def test_technical_visit_unauthenticated(self):
+        project = ProjectFactory.build()
+        get_initial_mock = {
+            'campaign': project.campaign,
+            'project': project.id,
+            'client': project.client,
+            'status': 'random',
+            'campaign_pk': 2
+        }
+        with patch.object(
+            TechnicalVisitView,
+            'get_initial',
+            return_value=get_initial_mock
+        ):
+            path = reverse('technical_visit', kwargs={'pk': 1})
+            request = RequestFactory().get(path)
+            request.user = AnonymousUser()
+
+            response = TechnicalVisitView.as_view()(request, pk=1)
+            assert 'auth/login' in response.url
