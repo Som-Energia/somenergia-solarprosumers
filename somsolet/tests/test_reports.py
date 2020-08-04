4f98e3443776cf6083c4acd2f20c5d371cc46c91
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2020-08-04 17:06:58 +0200

    fix test_reports

--- somsolet/tests/test_reports.py
+++ somsolet/tests/test_reports.py
@@ -1,9 +1,10 @@
-from datetime import datetime
+from datetime import date, datetime
+from unittest import mock
 
 import factory
 import pytest
 
-from scheduler_tasks import prereport_summary
+import scheduler_tasks
 from somsolet.models import Project
 
 from .factories import ProjectFactory
@@ -22,7 +23,7 @@ class TestSummaryReports:
             10,
             name=factory.Sequence(lambda n: 'TEST {0}'.format(n))
         )
-        summary = prereport_summary(Project.objects.filter(
+        summary = scheduler_tasks.prereport_summary(Project.objects.filter(
             name__contains='TEST'))
         assert summary == [
             {
@@ -34,35 +35,39 @@ class TestSummaryReports:
         ]
 
     def test_prereport_with_overdue(self):
-        ProjectFactory.create_batch(
-            5,
-            date_prereport=datetime.strptime('2020-01-01', '%Y-%m-%d'),
-            name=factory.Sequence(lambda n: 'TEST {0}'.format(n))
-        )
-        ProjectFactory.create_batch(
-            10,
-            name=factory.Sequence(lambda n: 'TEST {0}'.format(n))
-        )
-        ProjectFactory.create_batch(
-            2,
-            warning='prereport',
-            warning_date=datetime.strptime('2020-01-02', '%Y-%m-%d'),
-            name=factory.Sequence(lambda n: 'TEST {0}'.format(n))
-        )        
-        summary = prereport_summary(Project.objects.filter(
-            name__contains='TEST'))
-        assert summary == [
-            {
-                'name': 'Sent Prereports', 'value': 5
-            },
-            {
-                'name': 'Unsent Prereports', 'value': 12
-            },
-            {
-                'name': 'Overdue Prereports', 'value': 2
-            },
-            {
-                'name': 'Maximum overdue days',
-                'value': 103
-            }
-        ]
+
+        with mock.patch('scheduler_tasks.date') as datetime_patcher:
+            datetime_patcher.today.return_value = date(2020, 8, 3)
+        
+            ProjectFactory.create_batch(
+                5,
+                date_prereport=datetime.strptime('2020-01-01', '%Y-%m-%d'),
+                name=factory.Sequence(lambda n: 'TEST {0}'.format(n))
+            )
+            ProjectFactory.create_batch(
+                10,
+                name=factory.Sequence(lambda n: 'TEST {0}'.format(n))
+            )
+            ProjectFactory.create_batch(
+                2,
+                warning='prereport',
+                warning_date=datetime.strptime('2020-01-02', '%Y-%m-%d'),
+                name=factory.Sequence(lambda n: 'TEST {0}'.format(n))
+            )        
+            summary = scheduler_tasks.prereport_summary(Project.objects.filter(
+                name__contains='TEST'))
+            assert summary == [
+                {
+                    'name': 'Sent Prereports', 'value': 5
+                },
+                {
+                    'name': 'Unsent Prereports', 'value': 12
+                },
+                {
+                    'name': 'Overdue Prereports', 'value': 2
+                },
+                {
+                    'name': 'Maximum overdue days',
+                    'value': 214
+                }
+            ]
