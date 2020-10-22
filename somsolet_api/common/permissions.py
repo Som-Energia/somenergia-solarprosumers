b396c92d10529911f1e89e25ea263e9388bfd437
Benjami <benjami94@gmail.com>
2020-10-22 16:12:12 +0200

    Create SomsoletAPIModelPermissions

---
+++ somsolet_api/common/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import DjangoModelPermissions
+
+
+class SomsoletAPIModelPermissions(DjangoModelPermissions):
+
+    def __init__(self):
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
