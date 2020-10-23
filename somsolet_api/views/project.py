b8180201c55740fa46de218ec76ab50e9980e4fa
pol <pol.monso@somenergia.coop>
2020-10-23 17:11:34 +0200

    uncomment queryset code

--- somsolet_api/views/project.py
+++ somsolet_api/views/project.py
@@ -10,7 +10,6 @@ class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all().order_by('name')
     serializer_class = ProjectSerializer
 
-    # def get_queryset(self):
-    #     user = self.request.headers.get('dni')
-        
-    #     return Project.objects.filter(client__dni=user)
+    def get_queryset(self):
+        user = self.request.headers.get('dni')
+        return Project.objects.filter(client__dni=user)
