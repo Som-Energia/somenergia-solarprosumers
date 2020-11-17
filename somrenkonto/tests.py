4ab8c337bca31d66d8967c7afaf1832d1b66f076
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2020-11-17 10:07:17 +0100

    tests for TestFilterViewMixin class

--- somrenkonto/tests.py
+++ somrenkonto/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from django.contrib.auth import get_user_model
+from django.db.models import Q
 from django.test import Client
 from django.urls import reverse
 
@@ -31,9 +32,9 @@ class TestFilterViewMixin:
         ('event_type', str)
     ]
 
-    def test__get_filter_params(self, rf):
+    def test__get_filter_params__single_value(self, rf):
         base_url = reverse('somrenkonto')
-        url = f'{base_url}?campaign_id=1'
+        url = f'{base_url}?campaign_id=1&project_id=2'
         request = rf.get(url)
 
         filter_view_mixin = FilterViewMixin()
@@ -41,11 +42,11 @@ class TestFilterViewMixin:
 
         filter_params = filter_view_mixin.get_filter_params(request)
 
-        assert filter_params == {'campaign_id': [1]}
+        assert filter_params == [Q(campaign_id=1), Q(project_id=2)]
 
-    def test__get_filter_params(self, rf):
+    def test__get_filter_params__multiple_value(self, rf):
         base_url = reverse('somrenkonto')
-        url = f'{base_url}?campaign_id=1,2'
+        url = f'{base_url}?campaign_id=1,2&project_id=5,6'
         request = rf.get(url)
 
         filter_view_mixin = FilterViewMixin()
@@ -53,4 +54,32 @@ class TestFilterViewMixin:
 
         filter_params = filter_view_mixin.get_filter_params(request)
 
-        assert filter_params == {'campaign_id': [1]}
+        assert filter_params == [
+            Q(campaign_id__in=[1,2]), Q(project_id__in=[5,6])
+        ]
+
+    def test__get_filter_params__single_value_string_type(self, rf):
+        base_url = reverse('somrenkonto')
+        url = f'{base_url}?campaign_id=1&event_type=APPO'
+        request = rf.get(url)
+
+        filter_view_mixin = FilterViewMixin()
+        filter_view_mixin.FILTER_FIELDS = self.FILTER_FIELDS
+
+        filter_params = filter_view_mixin.get_filter_params(request)
+
+        assert filter_params == [Q(campaign_id=1), Q(event_type='APPO')]
+
+    def test__get_filter_params__multiple_value_srting_type(self, rf):
+        base_url = reverse('somrenkonto')
+        url = f'{base_url}?campaign_id=1,2&event_type=APPO,UNAVAIL'
+        request = rf.get(url)
+
+        filter_view_mixin = FilterViewMixin()
+        filter_view_mixin.FILTER_FIELDS = self.FILTER_FIELDS
+
+        filter_params = filter_view_mixin.get_filter_params(request)
+
+        assert filter_params == [
+            Q(campaign_id__in=[1,2]), Q(event_type__in=['APPO', 'UNAVAIL'])
+        ]
