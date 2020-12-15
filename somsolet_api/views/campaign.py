3dfbf903313bf18e8453ec5ae00d5f1fe3ff544e
Joana Figueira <joana.figueira@somenergia.coop>
2020-12-15 10:09:52 +0100

    auth_token working

--- somsolet_api/views/campaign.py
+++ somsolet_api/views/campaign.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from somsolet.models import Campaign
 from somsolet_api.serializer import CampaignSerializer
 from somsolet_api.common.permissions import SomsoletAPIModelPermissions
@@ -7,6 +8,7 @@ from somsolet_api.common.permissions import SomsoletAPIModelPermissions
 
 class CampaignViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
+    authentication_classes = [TokenAuthentication, SessionAuthentication]
 
     queryset = Campaign.objects.all().order_by('name')
     serializer_class = CampaignSerializer
