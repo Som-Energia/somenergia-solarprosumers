b308666c3adb9783a122acabd33f722cbcbade75
Marta <marta.fernandez@somenergia.coop>
2021-01-11 16:08:43 +0100

    wip: add RenkontoEvent view

--- somsolet_api/urls.py
+++ somsolet_api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import CampaignViewSet, ProjectViewSet, StagesViewSet
+from .views import (CampaignViewSet, ProjectViewSet, RenkontoEventViewSet,
+    StagesViewSet)
 
 router = routers.DefaultRouter()
 router.register(r'campaign', CampaignViewSet)
 router.register(r'project', ProjectViewSet)
 router.register(r'stages', StagesViewSet, basename='stages')
+router.register(r'event', RenkontoEventViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- somsolet_api/views/__init__.py
+++ somsolet_api/views/__init__.py
@@ -1,3 +1,4 @@
 from .campaign import CampaignViewSet
 from .project import ProjectViewSet
 from .stages import StagesViewSet
+from .event import RenkontoEventViewSet
