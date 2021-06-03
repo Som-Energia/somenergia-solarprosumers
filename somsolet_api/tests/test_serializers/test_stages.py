32be73e0e9ade6d12065c39fe91ae205163942b1
Marta <marta.fernandez@somenergia.coop>
2021-06-03 11:01:26 +0200

    add test_signature_file_serializer__with_data

--- somsolet_api/tests/test_serializers/test_stages.py
+++ somsolet_api/tests/test_serializers/test_stages.py
@@ -21,3 +21,22 @@ class TestSignatureFileSerializer:
             signed=False,
             status='empty status'
         )
+
+    @pytest.mark.django_db
+    def test_signature_file_serializer__with_data(self):
+        project = ProjectFactory()
+        project.id = 1
+        project.signature.check = True
+        project.status = 'signature'
+
+        signature_serializer = SignatureFileSerializer(
+            instance=project
+        )
+
+        assert signature_serializer.data == dict(
+            id=1,
+            signatureDate='2021-06-01',
+            signatureUpload=None,
+            signed=True,
+            status='signature'
+        )
