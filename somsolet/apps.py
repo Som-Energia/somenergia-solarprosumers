580a5538a389fe1189da09479bdcdca6335a1ba0
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2023-06-20 11:05:10 +0200

    🐛 fix AutoField warnig

--- somsolet/apps.py
+++ somsolet/apps.py
@@ -4,6 +4,7 @@ from django.apps import AppConfig
 
 class SomsoletConfig(AppConfig):
     name = "somsolet"
+    default_auto_field = "django.db.models.BigAutoField"
 
     def ready(self):
         scheduler = django_rq.get_scheduler("default")
