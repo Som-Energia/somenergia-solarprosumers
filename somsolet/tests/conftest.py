de830c06dde8548ff5690586701aab7737bcf21a
javikalsan <1070397+javikalsan@users.noreply.github.com>
2023-07-14 16:00:38 +0200

    ✅ Add missing connection parameters from settings
    Co-authored-by: Juan Pedro Sánchez <juanpe.sanchez@somenergia.coop>

--- somsolet/tests/conftest.py
+++ somsolet/tests/conftest.py
@@ -1,11 +1,20 @@
 import psycopg2
 import pytest
+
 from django.db import connections
+from django.conf import settings
+
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 
 def run_sql(sql):
-    conn = psycopg2.connect(database="postgres")
+    conn = psycopg2.connect(
+        database="postgres",
+        user=settings.DATABASES["default"]["USER"],
+        password=settings.DATABASES["default"]["PASSWORD"],
+        host=settings.DATABASES["default"]["HOST"],
+        port=settings.DATABASES["default"]["PORT"],
+    )
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = conn.cursor()
     cur.execute(sql)
@@ -14,8 +23,6 @@ def run_sql(sql):
 
 @pytest.yield_fixture(scope="session")
 def django_db_setup():
-    from django.conf import settings
-
     settings.DATABASES["default"]["NAME"] = "somsolet4test_db"
 
     run_sql("DROP DATABASE IF EXISTS somsolet4test_db")
