3d8d18c0da176af57215798f634a60c52a6d8e6d
javikalsan <1070397+javikalsan@users.noreply.github.com>
2023-07-20 10:44:39 +0200

    ✅ Add test__send_registration_email
    Co-authored-by: Juan Pedro Sánchez <juanpe.sanchez@somenergia.coop>

--- somsolet/tests/test_tasks.py
+++ somsolet/tests/test_tasks.py
@@ -1,9 +1,10 @@
 import pytest
 
 from unittest import TestCase
-from expects import expect
-from doublex import Spy, when
+from expects import expect, match
+from doublex import Spy
 from doublex_expects import have_been_called_with, have_been_called
+from somsolet.models import Project
 
 from somsolet.services import EmailService
 from somsolet.tasks import SendRegistrationTask
@@ -11,6 +12,32 @@ from somsolet.tests.factories import ProjectFactory, ClientFactory
 
 
 class TestTask(TestCase):
+    @pytest.mark.django_db
+    def test__send_registration_email(self):
+        project = ProjectFactory(registration_email_sent=False, client=ClientFactory())
+        email_service = Spy(EmailService)
+        sut = SendRegistrationTask(email_service)
+
+        sut.send_registration_email(project=project)
+
+        expect(email_service.send_email).to(
+            have_been_called_with(
+                to_email=[project.client.email],
+                subject="Confirmació d'Inscripció a la Compra Col·lectiva Som Energia",
+                message_params={
+                    "header": f"Hola {project.client.name},",
+                    "ending": f"Salut i bona energia,",
+                },
+                email_template="emails/message_body_general_conditions.html",
+                filename=match(
+                    "^.*Condicions_Generals_de_les_Compres_Col_lectives_de_(.*).pdf"
+                ),
+            )
+        )
+
+        last_saved_project = Project.objects.latest("id")
+        assert last_saved_project.registration_email_sent is True
+
     @pytest.mark.django_db
     def test__send_registration_email__already_sent(self):
         project = ProjectFactory(registration_email_sent=True, client=ClientFactory())
